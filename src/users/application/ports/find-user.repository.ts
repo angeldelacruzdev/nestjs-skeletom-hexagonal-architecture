06c1d763fe86acc8d89a7f1832a47f141708a4a7
Ángel De La Cruz <8610043+angeldelacruzdev@users.noreply.github.com>
2023-08-14 17:50:07 -0400

    Find By Email agregado en el puerto.

--- src/users/application/ports/find-user.repository.ts
+++ src/users/application/ports/find-user.repository.ts
@@ -3,4 +3,5 @@ import { UserResponseDto } from '../dtos';
 export interface FindUserRepositoryPort {
   findAll(): Promise<UserResponseDto[]>;
   findUserByid(id: number): Promise<UserResponseDto>;
+  findByEmail(email: string): Promise<UserResponseDto>;
 }
